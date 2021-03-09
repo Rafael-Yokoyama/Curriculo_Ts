dd82573e24435032bc52066df6af373a8e52d597
Rafael <rafaelyokoyama925@gmail.com>
2021-03-09 20:33:27 -0300

    build

--- src/components/Experience/index.tsx
+++ src/components/Experience/index.tsx
@@ -1,33 +1,49 @@
-
-import React from 'react'
-import Experiencia from '../../types/Experiencia'
-import { ChamarDadosTs } from '../../types/ChamarTs'
+import React from "react";
+import Experiencia from "../../types/Experiencia";
+import { ChamarDadosTs } from "../../types/ChamarTs";
 
 function Experience(props: ChamarDadosTs) {
-    const { experiencia } = props.resposta
+  const { experiencia } = props.resposta;
 
-    return (
-        <>
-            <div className="experience">
-                <h2>Experiência Profissional</h2>
+  return (
+    <>
+      <div className="experience">
+        <h2>Experiência Profissional</h2>
+        
 
-                {experiencia.map((item: Experiencia) => (
-                    <div className="experience-item" key={item.id}>
-                        <div>
-                            <h4>{item.cargo}</h4>
-                            <span>{item.periodo}</span>
+        {experiencia.map((item: Experiencia) => (
+          <div className="experience-item" key={item.id}>
+            <div>
+              <h4>{item.cargo}</h4>
+              <span>{item.periodo}</span>
 
-                            <strong>{item.empresa}</strong>
-                            <span className="final">{item.local}</span>
-                        </div>
-                        <p>
-                            {item.conteudo}
-                        </p>
-                    </div>
-                ))}
+              <strong>{item.empresa}</strong>
+              <span className="final">{item.local}</span>
             </div>
-        </>
-    )
+            <p>{item.conteudo}</p>
+          </div>
+        ))}
+
+
+          <h2>Experiência Profissional</h2>
+
+          
+          {experiencia.map((item: Experiencia) => (
+          <div className="experience-item" key={item.id}>
+            <div>
+              <h4>{item.cargo}</h4>
+              <span>{item.periodo}</span>
+
+              <strong>{item.empresa}</strong>
+              <span className="final">{item.local}</span>
+            </div>
+            <p>{item.conteudo}</p>
+          </div>
+        ))}
+        </div>
+     
+    </>
+  );
 }
 
-export default Experience
+export default Experience;
